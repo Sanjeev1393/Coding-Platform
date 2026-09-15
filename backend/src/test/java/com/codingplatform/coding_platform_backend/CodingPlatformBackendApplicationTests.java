f672f5e3bf6246076849a6bf9aafaf397e182c4a
Sanjeev Verma <vermasanjeev1393@gmail.com>
2026-09-15 23:33:35 +0530

    Add Spring Boot backend foundation

---
+++ backend/src/test/java/com/codingplatform/coding_platform_backend/CodingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingplatform.coding_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
